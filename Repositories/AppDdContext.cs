62e54595ab142316f94fbf5d25e391e011a95ba7
Arda <ardaeray20059@gmail.com>
2025-07-04 11:46:01 +0300

    Migration Done

---
+++ Repositories/AppDdContext.cs
@@ -0,0 +1,21 @@
+﻿using App.Repositories.Products;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Diagnostics.Metrics;
+using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace App.Repositories
+{
+    public class AppDdContext(DbContextOptions<AppDdContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
